7a40d64d9149b980b9eb8317b49e49d2463855c9
cameliadorhoi <52788556+cameliadorhoi@users.noreply.github.com>
2019-08-29 16:33:14 +0300

    first commit

---
+++ Page object Model/Test11-17aug.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using OpenQA.Selenium.Support.UI;
+
+namespace NUnitTestProjectTraining.Page_object_Model
+{
+    class Test11_17aug
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void initialization()
+        {
+            driver = new ChromeDriver("C:\\Users\\cameliadorhoi\\source\\repos\\PageObjectModel\\bin\\Debug\\netcoreapp2.1");
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?id_product=1&controller=product");
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(10);
+
+            var group_1 = driver.FindElement(By.Id("group_1"));
+            var select_group_1 = new SelectElement(group_1);
+            select_group_1.SelectByValue("3");
+
+  
+        }
+
+  
+
+    }
+
+}
